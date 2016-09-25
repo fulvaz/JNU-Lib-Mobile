39877d9eaf3b6f323d7dfd5da91bfa6cc3c40e20
fulvaz <fulvaz@foxmail.com>
2016-09-25 20:18:03 +0800

    start testting

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -15,12 +15,19 @@
     export default{
         data(){
             return{
-
+                books: []
             }
         },
         route: {
             // 我来尝尝语法糖
-
+            data: function({to: {params: query}}) {
+                var result = new Result();
+                return result.fetch(query)
+                    .then(function(data) {
+                        data = JSON.parse(data);
+                        this.books = data;
+                    });
+            }
         },
         components:{
         }
--- src/model/Result.js
+++ src/model/Result.js
@@ -8,10 +8,14 @@ function Result() {
 Result.prototype.fetch = function(query) {
   var promise = new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', config.searchURL + query);
+    xhr.open('GET', config.searchURL); // TODO fix this!
     xhr.setRequestHeader('Accetpt-Charset', 'utf-8');
     xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onload = () => resolve();
+    xhr.onload = () => {
+      "use strict";
+      resolve(xhr.responseText);
+    };
     xhr.send();
-  })
-}
+  });
+  return promise;
+};
